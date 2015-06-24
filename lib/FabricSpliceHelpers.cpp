db7d8950a76e3f5c231b5d53cd8caf21d53d16c4
Andrew MacPherson <andrew.macp@gmail.com>
2015-06-24 19:12:26 +0200

    UI changes for licensing, FE-4014.

--- lib/FabricSpliceHelpers.cpp
+++ lib/FabricSpliceHelpers.cpp
@@ -2,6 +2,8 @@
 #include "FabricSpliceHelpers.h"
 #include <DFG/DFGLogWidget.h>
 #include <DFG/DFGCombinedWidget.h>
+#include <Licensing/Licensing.h>
+#include <FabricSplice.h>
 
 #include <maya/MGlobal.h>
 
@@ -83,11 +85,28 @@ void mayaCompilerErrorFunc(unsigned int row, unsigned int col, const char * file
   FabricUI::DFG::DFGLogWidget::log(composed.asChar());
 }
 
-void mayaKLStatusFunc(const char * topic, unsigned int topicLength,  const char * message, unsigned int messageLength)
+void mayaKLStatusFunc(const char * topicData, unsigned int topicLength,  const char * messageData, unsigned int messageLength)
 {
-  MString composed = MString("[KL Status]: ")+MString(message);
+  MString composed = MString("[KL Status]: ")+MString(messageData, messageLength);
   MGlobal::displayInfo(composed);
-  FabricUI::DFG::DFGLogWidget::log(composed.asChar());
+
+  FTL::StrRef topic( topicData, topicLength );
+  FTL::StrRef message( messageData, messageLength );
+  FabricCore::Client *client =
+    const_cast<FabricCore::Client *>( FabricSplice::DGGraph::getClient() );
+  if ( topic == FTL_STR( "licensing" ) )
+  {
+    try
+    {
+      FabricUI::HandleLicenseData( NULL, *client, message );
+    }
+    catch ( FabricCore::Exception e )
+    {
+      FabricUI::DFG::DFGLogWidget::log(e.getDesc_cstr());
+    }
+  }
+  else
+    FabricUI::DFG::DFGLogWidget::log(composed.asChar());
 }
 
 void mayaRefreshFunc()
