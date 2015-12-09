14f0c770c9550c74cdbd618afd802b897b729975
Eric Mootz <eric@mootzoid.com>
2015-12-09 15:31:48 +0100

    FE-5810 check the node state (Normal, HasNoEffect,...) and behave accordingly

--- lib/FabricDFGMayaNode.cpp
+++ lib/FabricDFGMayaNode.cpp
@@ -56,22 +56,38 @@ MStatus FabricDFGMayaNode::compute(const MPlug& plug, MDataBlock& data){
   _outputsDirtied = false;
   
   MStatus stat;
-  
-  MAYADFG_CATCH_BEGIN(&stat);
 
-  FabricSplice::Logging::AutoTimer timer("Maya::compute()");
+  // get the node's state data handle.
+  MDataHandle stateData = data.outputValue(state, &stat);
+  if (stat != MS::kSuccess)
+    return stat;
 
-  // if(!_spliceGraph.checkErrors()){
-  //   return MStatus::kFailure; // avoid evaluating on errors
-  // }
-
-  if(transferInputValuesToDFG(data))
+  if (stateData.asShort() == 0)       // 0: Normal.
   {
-    evaluate();
-    transferOutputValuesToMaya(data);
-  }
+    MAYADFG_CATCH_BEGIN(&stat);
 
-  MAYADFG_CATCH_END(&stat);
+    FabricSplice::Logging::AutoTimer timer("Maya::compute()");
+
+    // if(!_spliceGraph.checkErrors()){
+    //   return MStatus::kFailure; // avoid evaluating on errors
+    // }
+
+    if(transferInputValuesToDFG(data))
+    {
+      evaluate();
+      transferOutputValuesToMaya(data);
+    }
+
+    MAYADFG_CATCH_END(&stat);
+  }
+  else if (stateData.asShort() == 1)  // 1: HasNoEffect.
+  {
+    stat = MS::kNotImplemented;
+  }
+  else                                // not supported by Canvas node.
+  {
+    stat = MS::kNotImplemented;
+  }
 
   return stat;
 }
