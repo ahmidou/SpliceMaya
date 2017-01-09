9ac63e42865d690fd26ead43f39c2eeb39a4ee5b
Julien Couet <couet.julien@gmail.com>
2017-01-09 16:45:03 +0100

    Set port options read-only when it's been created already

--- lib/FabricDFGWidget.cpp
+++ lib/FabricDFGWidget.cpp
@@ -208,14 +208,14 @@ void FabricDFGWidget::onPortEditDialogCreated(DFG::DFGBaseDialog * dialog)
   DFG::DFGEditPortDialog * editPortDialog = (DFG::DFGEditPortDialog *)dialog;
   QString title = editPortDialog->title();
 
-  // FE-6747 : Don't edit the port if it's been set already.
+  // FE-6747/FE-7369 : Don't edit the port if it's been set already.
   if(!editPortDialog->dataType().isEmpty())
+  {
     editPortDialog->setDataTypeReadOnly(true);
-
-  // FE-7369 :
-  editPortDialog->setSoftRangeReadOnly(true);
-  editPortDialog->setHardRangeReadOnly(true);
-  editPortDialog->setComboReadOnly(true);
+    editPortDialog->setSoftRangeReadOnly(true);
+    editPortDialog->setHardRangeReadOnly(true);
+    editPortDialog->setComboReadOnly(true);
+  }
 
   if(editPortDialog->dataType().isEmpty())
     editPortDialog->setVisibilityReadOnly(true);
